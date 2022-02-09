1ce543cfe4361651f234dab3ad0ab6b291b0da04
BuildTools <unconfigured@null.spigotmc.org>
2022-02-09 02:32:02 -0600

    Add project files.

---
+++ PriceQuotationApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuotationApp.Models;
+
+namespace PriceQuotationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.discountAmount = 0;
+            ViewBag.Total = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.discountAmount = model.DiscountAmount();
+                ViewBag.Total = model.TotalAmount();
+            }
+            else {
+                ViewBag.discountAmount = 0;
+                ViewBag.Total = 0;
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ PriceQuotationApp/Models/PriceQuotationModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotationApp.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage ="Please enter a sale price.")]
+        [Range(0.01, 500, ErrorMessage = "Sale price must be greater than zero and less or equal to 500")]
+        public decimal? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount percent.")]
+        [Range(0.0, 100.0, ErrorMessage = "Discount percent must be between 0 and 100")]
+        public decimal? DiscountPercent { get; set; }
+
+        public decimal? DiscountAmount() {
+            decimal? discountAmountCal = Subtotal * (DiscountPercent / 100);
+            
+            return discountAmountCal;
+        }
+
+        public decimal? TotalAmount() {
+            decimal? totalAmountCal = Subtotal - (Subtotal * (DiscountPercent /100));
+
+            return totalAmountCal;
+        }
+    }
+}
